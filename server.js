46ea4d51ca5bf75bea5aca6f6245675010d35ac9
Samantha Orozco <orozcoxs@gmail.com>
2023-03-28 19:09:11 -0700

    api's added

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./RouteFiles/API-route')(app);
-require('./RouteFiles/Html-routes')(app);
-
+require('./routes/api-route')(app);
+require('./routes/html-route')(app);
 app.listen(PORT, function() {
     console.log('Application listening http://localhost:' + PORT);
 });
