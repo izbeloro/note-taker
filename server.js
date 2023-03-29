2455d340d0400a2ec7aed9e5475aed81c68c8cc2
Samantha Orozco <orozcoxs@gmail.com>
2023-03-28 19:11:24 -0700

    file name changed to fix bug

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/api-route')(app);
-require('./routes/html-route')(app);
+require('./routeFiles/api-route')(app);
+require('./routeFiles/html-route')(app);
 app.listen(PORT, function() {
     console.log('Application listening http://localhost:' + PORT);
 });
